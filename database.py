6c4cad1c1ecbf149f61787a2bc6187e77280d11c
Kuldeep Verma <109075089+KD-007@users.noreply.github.com>
2025-04-26 11:34:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,27 @@
+import pyodbc
 import os
-import pymssql
+
+
+# Fetch the variables
+DB_SERVER = os.getenv('DB_SERVER')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+
+conn_str = (
+    f'DRIVER={{SQL Server}};'
+    f'SERVER={DB_SERVER};'
+    f'DATABASE={DB_NAME};'
+    f'UID={DB_USER};'
+    f'PWD={DB_PASSWORD};'
+)
+
+
 
 def get_db_connection():
-    return pymssql.connect(
-        server=os.getenv('DB_SERVER'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME'),
-        as_dict=True  # Returns results as dictionaries
-    )
+    try:
+        conn = pyodbc.connect(conn_str)
+        return conn
+    except Exception as e:
+        raise e
